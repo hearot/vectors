48bcb84964398d873a8aaa3fde5e949c2253490a
Hearot <gabriel@hearot.it>
2020-04-08 09:53:28 +0200

    vectors: add polar & azimuthal angle

--- vectors.py
+++ vectors.py
@@ -5,10 +5,23 @@ i = Symbol('i')
 j = Symbol('j')
 k = Symbol('k')
 
+def convert_to_sympy(k):
+    if isinstance(k, int):
+        return sp.Integer(k)
+    elif isinstance(k, float):
+        return sp.Float(k)
+    
+    return k
+
+
 class Vector:
     vector = 0
     
     def __init__(self, x=0, y=0, z=0):
+        x = convert_to_sympy(x)
+        y = convert_to_sympy(y)
+        z = convert_to_sympy(z)
+        
         self.vector = x*i + y*j + z*k
         
     def __add__(self, u):
@@ -19,6 +32,9 @@ class Vector:
             return self.dot(u)
         else:
             return Vector.from_symbols(u*self.vector)
+        
+    def __or__(self, u):
+        return sp.acos(self*u/(self.module*u.module))
             
     def __pow__(self, u):
         return self.cross(u)   
@@ -40,9 +56,13 @@ class Vector:
     
     def __sub__(self, u):
         return Vector.from_symbols(self.vector-u.vector)
+    
+    def __xor__(self, u):
+        return sp.asin((self**u).module/(self.module*u.module))
 
-    def bi_angle(self):
-        return sp.atan(sp.Rational(self.vector.coeff(j), self.vector.coeff(i)))
+    @property
+    def azimuthal(self):
+        return sp.atan2(self.vector.coeff(j), self.vector.coeff(i))
 
     def cross(self, u):
         return Vector.from_symbols(
@@ -58,14 +78,33 @@ class Vector:
                 self.vector.coeff(k)*u.vector.coeff(k))
         
     @staticmethod
+    def from_polar(r, theta, phi):
+        r = convert_to_sympy(r)
+        theta = convert_to_sympy(theta)
+        phi = convert_to_sympy(phi)
+        
+        return Vector(
+            r*sp.sin(theta)*sp.cos(phi),
+            r*sp.sin(theta)*sp.sin(phi),
+            r*sp.cos(theta)
+        )
+        
+    @staticmethod
     def from_symbols(v):
         v = v.expand()
         return Vector(v.coeff(i), v.coeff(j), v.coeff(k))    
 
+    @property
     def module(self):
         return sp.sqrt(self.vector.coeff(i)**2 +
                        self.vector.coeff(j)**2 +
                        self.vector.coeff(k)**2)
+        
+    @property
+    def polar(self):
+        return sp.asin(
+            sp.sqrt(self.vector.coeff(i)**2 +
+                    self.vector.coeff(j)**2)/self.module)
 
 
 v_x, v_y, v_z = symbols('v_x, v_y, v_z')
